f81129b8dc8999a722792f4668e0db9ce580780e
Ernest Haliievskyi <95231522+Erhal@users.noreply.github.com>
2022-10-06 15:36:26 +0400

    Update todos.js

--- react-dz-5/src/store/api/todos.js
+++ react-dz-5/src/store/api/todos.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 export const todosApi = createApi({
     reducerPath: 'todosApi',
     baseQuery: fetchBaseQuery(
-        {baseUrl: 'https://jsonplaceholder.typicode.com/'}
+        {baseUrl: 'https://dummyjson.com/'}
     ),
     endpoints: (builder) => ({
         getAllTodos: builder.query({
