a4cecd473f031b816e33d4d1d99993124044522c
Erhal <odhernes@gmail.com>
2022-11-15 01:54:55 +0400

    Fix

--- react-dz-8/src/components/forms/RegistrationForm/index.jsx
+++ react-dz-8/src/components/forms/RegistrationForm/index.jsx
@@ -164,11 +164,11 @@ const RegistrationForm = () => {
                 <div className='d-flex gap-2'>
                     <Form.Group className="w-50 mb-4" controlId="formCitySelect">
                         <Form.Label>City</Form.Label>
-                        <CitiesSelect control={control} name={'city'} error={errors?.city}/>
+                        <CitiesSelect control={control} name={'city'}/>
                     </Form.Group>
                     <div className="w-50 mb-3 d-flex justify-content-center align-items-center">
                         <Button variant="outline-success" type="submit" className="w-100">
-                            Submit
+                            Sign Up
                         </Button>
                     </div>
                 </div>
